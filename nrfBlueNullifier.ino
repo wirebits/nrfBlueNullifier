5c84f6084c79c7305b82cc0e6141476f6d4039e4
WireBits <159493381+wirebits@users.noreply.github.com>
2024-11-04 13:03:09 +0530

    Update nrfBlueNullifier.ino

--- nrfBlueNullifier.ino
+++ nrfBlueNullifier.ino
@@ -6,45 +6,50 @@
 
 #include <RF24.h>
 #include <esp_bt.h>
+#include <esp_bt_main.h>
 
 const int switchPin = 12;
 SPIClass *sp = nullptr;
 RF24 radio(22, 21, 19909090);
-byte i = 45;
-unsigned int flag = 0;
+byte i = 45, ptr_hop = 0, flag = 0;
+byte hopping_channel[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80, 82, 84, 86};
 
 void nrfSPIInit() {
- sp = new SPIClass(VSPI);
- sp->begin();
- if (radio.begin(sp)) {
-  radio.setAutoAck(false);
-  radio.stopListening();
-  radio.setRetries(0, 0);
-  radio.setPayloadSize(31);
-  radio.setAddressWidth(4);
-  radio.setPALevel(RF24_PA_MAX, true);
-  radio.setDataRate(RF24_2MBPS);
-  radio.setCRCLength(RF24_CRC_DISABLED);
-  radio.printPrettyDetails();
-  radio.startConstCarrier(RF24_PA_MAX, i);
- }
+    sp = new SPIClass(VSPI);
+    sp->begin();
+    if (radio.begin(sp)) {
+        radio.setAutoAck(false);
+        radio.stopListening();
+        radio.setRetries(0, 0);
+        radio.setPayloadSize(31);
+        radio.setAddressWidth(4);
+        radio.setPALevel(RF24_PA_MAX, true);
+        radio.setDataRate(RF24_2MBPS);
+        radio.setCRCLength(RF24_CRC_DISABLED);
+        radio.startConstCarrier(RF24_PA_MAX, i);
+    }
 }
 
 void adjustAndSweepChannels() {
- flag = (i > 79) ? 1 : ((i < 2) ? 0 : flag);
- i += (flag == 0) ? 2 : -2;
- radio.setChannel(i);
- for (int j = 0; j <= 79; j++) radio.setChannel(j);
+    flag = (i > 79) ? 1 : (i < 2 ? 0 : flag);
+    i += flag ? -2 : 2;
+    for (int j = 0; j <= 79; j++) radio.setChannel(j);
 }
 
-void setup(){
- pinMode(switchPin, INPUT_PULLUP);
- esp_bt_controller_deinit();
- nrfSPIInit();
+void setup() {
+    pinMode(switchPin, INPUT_PULLUP);
+    esp_bt_controller_deinit();
+    if (esp_bluedroid_get_status() == ESP_BLUEDROID_STATUS_ENABLED) {
+        esp_bluedroid_disable();
+        esp_bluedroid_deinit();
+    }
+    nrfSPIInit();
 }
 
-void loop(){
- if (digitalRead(switchPin) == LOW){
-  adjustAndSweepChannels();
- }
+void loop() {
+    if (!digitalRead(switchPin)) {
+        adjustAndSweepChannels();
+        ptr_hop = (ptr_hop + 1) % sizeof(hopping_channel);
+        radio.setChannel(hopping_channel[ptr_hop]);
+    }
 }
